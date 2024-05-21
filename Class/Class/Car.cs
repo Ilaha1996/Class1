9d96506462fbafec23f684f78e59e3b3be276603
Ilaha <ilahaah@code.edu.az>
2024-05-21 17:18:51 +0400

    Add files via upload

---
+++ Class/Class/Car.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    public class Car : Vechile
+    {
+        public double FuelCapacity;
+        public double FuelPerKm;
+
+        public void Drive(int km)
+        {
+            Console.WriteLine(FuelCapacity - FuelPerKm * km);
+            FuelCapacity = FuelCapacity - FuelPerKm * km;
+        }
+
+        public void ShowFullData()
+        {
+            Console.WriteLine($"{WheelCount} {Color} {FuelCapacity} {FuelPerKm}");
+
+
+        }
+
+        public string GetFullData() 
+        {
+
+
+            return $"{WheelCount} {Color} {FuelCapacity} {FuelPerKm}";
+        }
+
+
+
+
+
+    }
+}
---
+++ Class/Class/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Car car1= new Car();
+            car1.WheelCount = 6;
+            car1.Color = "Green";
+            car1.FuelCapacity = 200;
+            car1.FuelPerKm = 5;
+            car1.Drive(10);
+            car1.ShowFullData();
+            Console.WriteLine(car1.GetFullData());  
+        }
+    }
+}   
